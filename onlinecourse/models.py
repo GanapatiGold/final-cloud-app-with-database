66882d8827b581b5229abbf478bc2ff9f4397593
GanapatiGold <ivankhalo97@gmail.com>
2023-05-30 20:46:44 -0400

    models and migrations act

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -76,7 +76,9 @@ class Lesson(models.Model):
     content = models.TextField()
 
 
-
+# Enrollment model
+# <HINT> Once a user enrolled a class, an enrollment entry should be created between the user and course
+# And we could use the enrollment to track information such as exam submissions
 class Enrollment(models.Model):
     AUDIT = 'audit'
     HONOR = 'honor'
@@ -93,6 +95,8 @@ class Enrollment(models.Model):
     rating = models.FloatField(default=5.0)
 
 
+    
+
 class Question(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, default="")
     question_text = models.CharField(max_length=500, default="")
@@ -106,11 +110,18 @@ class Question(models.Model):
        else:
            return False
 
-    class Choice(models.Model):
+
+
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=300, default="")
     is_correct = models.BooleanField(default=False)
 
+# <HINT> The submission model
+# One enrollment could have multiple submission
+# One submission could have multiple choices
+# One choice could belong to multiple submissions
 class Submission(models.Model):
    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
    choices = models.ManyToManyField(Choice)
+#    Other fields and methods you would like to design
